4dbac216bc6e1eefbb922e6cbbde296e43097631
Hanif Uddin <hanif.inno@pakizasoftware.com>
2025-08-16 19:13:01 +0600

    locally fetched feed data

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,10 +1,37 @@
-import 'dart:async';
-
 import 'package:get/get.dart';
-import 'package:meme_verse/app/data/login_credentials.dart';
-
-import '../../../routes/app_pages.dart';
+import 'package:meme_verse/app/core/models/meme_model.dart';
 
 class HomeController extends GetxController {
-  LoginCredential loginCredential = LoginCredential();
+  var currentIndex = 0.obs;
+  var feedList = <MemeModel>[].obs;
+
+  void changeTab(int index) {
+    currentIndex.value = index;
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    // Fetch feed when controller initializes
+    refreshFeed();
+  }
+
+  Future<void> refreshFeed() async {
+    // TODO: Fetch memes from backend or local storage
+    await Future.delayed(const Duration(seconds: 1));
+    feedList.assignAll([
+      MemeModel(
+        id: '1',
+        imageUrl: 'https://i.imgflip.com/1ur9b0.jpg',
+        likeCount: 10,
+        title: 'Funny Meme 1',
+      ),
+      MemeModel(
+        id: '2',
+        imageUrl: 'https://i.imgflip.com/26am.jpg',
+        likeCount: 25,
+        title: 'Funny Meme 2',
+      ),
+    ]);
+  }
 }
