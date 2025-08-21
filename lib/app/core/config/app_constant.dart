5d2cf6f706970cc52e9663c31057eb2e97885ce4
Hanif Uddin <hanif.inno@pakizasoftware.com>
2025-08-21 12:23:09 +0600

    supabase anon key and supabase url added

--- lib/app/core/config/app_constant.dart
+++ lib/app/core/config/app_constant.dart
@@ -5,5 +5,10 @@ class AppConstant {
   static const String APP_OWNER_NAME = 'Hanif Uddin';
   static const String SERVER_CLIENT_ID =
       '700947391250-4a53hveuhh0sujr61i82enu0lbp15ns2.apps.googleusercontent.com';
+
+  static const String SUPABASE_ANON_KEY =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpY2lwaWhlbWRpZ3BzdGhuaWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3NDY2NDAsImV4cCI6MjA3MTMyMjY0MH0.IgA4Ezjdcx-7iGKhdqsfCRQdZobX3K4F2hYJiDbE3S0';
+
+  static const String SUPABASE_URL = 'https://cicipihemdigpsthnibk.supabase.co';
 }
 //700947391250-4a53hveuhh0sujr61i82enu0lbp15ns2.apps.googleusercontent.com
