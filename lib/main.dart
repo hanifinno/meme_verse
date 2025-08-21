369abe28bf92d5f51a36386e55e8472ae261ed07
Hanif Uddin <hanif.inno@pakizasoftware.com>
2025-08-21 12:22:38 +0600

    supabase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:meme_verse/app/core/config/app_constant.dart';
 import 'package:meme_verse/app/core/theme/theme_service/theme_service.dart';
 import 'package:meme_verse/firebase_options.dart';
 import 'package:meme_verse/meme_verse.dart';
 import 'package:meme_verse/app/utils/loader.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Supabase.initialize(
+    url: AppConstant.SUPABASE_URL, // 👈 Project URL
+    anonKey: AppConstant.SUPABASE_ANON_KEY, // 👈 anon key
+  );
   await GetStorage.init();
   configLoader();
   // Initialize ThemeService
